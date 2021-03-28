dd785dc9f6dbe57aec292fc0f9673946b1d45911
jenci <68970896+jenci2114@users.noreply.github.com>
2021-03-28 15:46:37 -0700

    Implemented conversions between wxf and coordinate

--- chess_game.py
+++ chess_game.py
@@ -4,6 +4,9 @@ from __future__ import annotations
 from typing import Optional
 
 
+_MAX_MOVES = 200
+
+
 class ChessGame:
     """A class representing a state of a game of Chinese Chess.
     """
@@ -79,7 +82,157 @@ class ChessGame:
             self._move_count = move_count
             self._valid_moves = []
 
-            self._recalculate_valid_moves  # to be implemented
+            # self._recalculate_valid_moves  # to be implemented
+
+    def get_valid_moves(self) -> list[str]:
+        """Return a list of the valid moves for the active player."""
+        return self._valid_moves
+
+    def make_move(self, move: str) -> None:
+        """Make the given chess move. This instance of ChessGame will be mutated, and will
+        afterwards represent the game state after move is made.
+
+        If move is not a currently valid move, raise a ValueError.
+        """
+        if move not in self._valid_moves:
+            raise ValueError(f'Move "{move}" is not valid')
+
+        self._board = self._board_after_move(move)
+
+        self._is_red_active = not self._is_red_active
+        self._move_count += 1
+
+        self._recalculate_valid_moves()
+
+    def copy_and_make_move(self, move: str) -> ChessGame:
+        """Make the given chess move in a copy of this ChessGame, and return that copy.
+
+        If move is not a currently valid move, raise a ValueError.
+        """
+        return ChessGame(board=self._board_after_move(move),
+                         red_active=not self._is_red_active,
+                         move_count=self._move_count + 1)
+
+    def is_red_move(self) -> bool:
+        """Return whether the red player is to move next."""
+        return self.is_red_move()
+
+    def get_winner(self) -> Optional[str]:
+        """Return the winner of the game (red or black) or 'draw' if the game ended in a draw.
+
+        Return None if the game is not over.
+        """
+        if self._move_count >= _MAX_MOVES:
+            return 'Draw'
+        elif ...:
+            ...  # Implement later
+
+    def _calculate_moves_for_board(self, board: list[list[Optional[_Piece]]],
+                                   is_red_active: bool) -> tuple:
+        """Return all posible moves on a given board with a given active player."""
+        moves = []
+        # Used to calculate whether the other players' king is in check
+        # (i.e. the black king if is_red_active, otherwise the red king)
+        check = []
+
+        for pos in [(y, x) for y in range(0, 10) for x in range(0, 9)]:
+            piece = board[pos[0]][pos[1]]
+            if piece is None or piece.is_red != is_red_active:
+                continue
+
+            kind, is_red = piece.kind, piece.is_red
+
+            if kind == 'r':
+                ...  # implement later
+
+    def _find_moves_in_direction(self, board, moves, pos, is_red, direction, limit=None,
+                                 capture=None):
+        """Find valid moves moving in a given direction from a certain position.
+
+        capture: True if must capture, False if must not capture, None otherwise.
+        """
+        move_start = ...  # implement later
+
+    def _wxf_to_index(self, piece: str, is_red: bool) -> tuple[int, int]:
+        """Return the coordinate of a piece (y, x) given the wxf notation for a piece (e.g. 'c6').
+
+        Raise ValueError if not found.
+        """
+        piece_lower = piece.lower()
+        type = piece_lower[0]
+        location = piece_lower[1]
+        if location in {'+', '-'}:
+            y, x = 0, 0
+
+            # Find the first piece
+            while y <= 9 and self._board[y][x] != _Piece(type, is_red):
+                x += 1
+                if x >= 9:
+                    y += 1
+                    x = 0
+
+            if y > 9:
+                raise ValueError
+
+            coord1 = (y, x)
+            y += 1
+
+            # Find the second piece (they are in the same column)
+            while y <= 9 and self._board[y][x] != _Piece(type, is_red):
+                y += 1
+
+            if y > 9:
+                raise ValueError
+
+            coord2 = (y, x)  # y is greater than coord1
+
+            if (is_red and location == '+') or (not is_red and location == '-'):
+                return coord1
+            else:
+                return coord2
+        else:  # location is a number
+            if is_red:
+                x = 9 - int(location)
+            else:  # not is_red
+                x = int(location) - 1
+
+            y = 0
+            # Find the piece in the given column
+            while y <= 9 and self._board[y][x] != _Piece(type, is_red):
+                y += 1
+
+            if y > 9:
+                raise ValueError
+            else:
+                return (y, x)
+
+    def _index_to_wxf(self, pos: tuple[int, int], is_red: bool) -> str:
+        """Return the wxf notation of a piece (e.g. 'c6') given its coordinate (y, x).
+
+        Raise ValueError is there is no piece at the position or the piece has the opposite colour.
+        """
+        piece = self._board[pos[0]][pos[1]]
+        if piece is None or piece.is_red != is_red:
+            raise ValueError
+
+        type = piece.kind
+        x = pos[1]
+
+        # Search if another piece is present in the column
+        y = 0
+        while y <= 9 and (self._board[y][x] != _Piece(type, is_red) or y == pos[0]):
+            y += 1
+
+        if y > 9:
+            if is_red:
+                return type + str(9 - x)
+            else:  # not is_red
+                return type + str(x + 1)
+        else:
+            if (is_red and pos[0] > y) or (not is_red and pos[0] < y):
+                return type + '-'
+            else:
+                return type + '+'
 
 
 class _Piece:
@@ -106,3 +259,8 @@ class _Piece:
         """Initialize a new piece."""
         self.kind = kind
         self.is_red = is_red
+
+    def __eq__(self, other: Optional[_Piece]) -> bool:
+        if other is None:
+            return False
+        return self.kind == other.kind and self.is_red == other.is_red
