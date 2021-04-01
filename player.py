5075de827730e4c073cda1f044f71590185e7e9f
Junru <71200653+TracyJL@users.noreply.github.com>
2021-04-02 01:49:22 +0800

    Add new players

--- player.py
+++ player.py
@@ -41,7 +41,7 @@ class RandomPlayer(Player):
 
 
 class RandomTreePlayer(Player):
-    """A Chinese player that plays randomly based on a given GameTree.
+    """A Chinese Chess player that plays randomly based on a given GameTree.
 
     This player uses a game tree to make moves, descending into the tree as the game is played.
     On its turn:
@@ -100,3 +100,157 @@ class RandomTreePlayer(Player):
             subtrees = self._game_tree.get_subtrees()
             self._game_tree = random.choice(subtrees)
             return self._game_tree.move
+
+
+class GreedyTreePlayer(Player):
+    """A Chinese Chess player that chooses a move based on the database,
+    using Minimax algorithm.
+
+    If the player is red, then it will choose a move that has highest winning
+    probability for red.
+
+    If the player is black, then it will choose a move that has lowest winning
+    probability for red.
+
+    """
+    # Private Instance Attributes:
+    #   - _game_tree:
+    #       The GameTree that this player uses to make its moves. If None, then this
+    #       player just makes random moves.
+    _game_tree: Optional[GameTree]
+
+    def __init__(self, game_tree: GameTree) -> None:
+        """Initialize this player.
+
+        Preconditions:
+            - game_tree represents a game tree at the initial state (root is '*')
+        """
+        self._game_tree = game_tree
+
+    def make_move(self, game: ChessGame, previous_move: Optional[str]) -> str:
+        """Make a move given the current game.
+
+        previous_move is the opponent player's most recent move, or None if no moves
+        have been made.
+
+        Preconditions:
+            - There is at least one valid move for the given game
+        """
+        if self._game_tree is not None and previous_move is not None:
+            found = 0
+
+            for subtree in self._game_tree.get_subtrees():
+                if subtree.move == previous_move:
+                    self._game_tree = subtree
+                    found = 1  # subtree is found
+                    break
+
+            if found == 0:  # no subtree is found
+                self._game_tree = None
+
+        if self._game_tree is None or self._game_tree.get_subtrees() == []:
+            # no choice for move so we make a random move
+            possible_moves = game.get_valid_moves()
+            self._game_tree = None
+            return random.choice(possible_moves)
+        else:
+            subtrees = self._game_tree.get_subtrees()
+            white_win = [sub.red_win_probability for sub in subtrees]
+            if self._game_tree.is_red_move:
+                maximum = max(white_win)
+                maximum_index = white_win.index(maximum)
+                max_subtree = subtrees[maximum_index]
+                self._game_tree = max_subtree
+            else:  # if playing as black
+                minimum = min(white_win)
+                minimum_index = white_win.index(minimum)
+                min_subtree = subtrees[minimum_index]
+                self._game_tree = min_subtree
+
+            return self._game_tree.move
+
+
+class ExploringPlayer(Player):
+    """A Chinese Chess player that uses alpha-beta algorithm to explore all possible moves
+     and find a locally optimal move.
+
+     Note: This player does not need an existing tree to select a moves from.
+     Instead, it will explore all recent moves and choose a locally optimal.
+
+    """
+    # Private Instance Attributes:
+    #   - _depth: the number of turns the player will explore
+    _depth: int
+
+    def __init__(self, depth: int) -> None:
+        """Initialize this player.
+
+        Preconditions:
+            - depth >= 1
+        """
+        self._depth = depth
+
+    def make_move(self, game: ChessGame, previous_move: Optional[str]) -> str:
+        """Make a move given the current game.
+
+        previous_move is the opponent player's most recent move, or None if no moves
+        have been made.
+
+        Preconditions:
+            - There is at least one valid move for the given game
+        """
+        # TODO: implement the method
+
+
+class LearningPlayer(Player):
+    """A Chinese Chess player that uses alpha-beta algorithm to explore some possible moves
+     based on its experience and find a locally optimal move. It's a player which has some
+     previous experience in Chinese Chess but has its own idea to explore a better strategy.
+
+     The new moves this player will explore are...
+
+     Note: This player should not be totally new to Chinese Chess. It has some previous
+     experience in Chinese Chess, and therefore its game tree should not be empty.
+
+    """
+    # Private Instance Attributes:
+    #   - _depth: the number of turns the player will explore
+    #   - _game_tree:
+    #       The GameTree that this player uses to make its moves. If None, then this
+    #       player just makes random moves.
+    _depth: int
+    _game_tree: GameTree
+
+    def __init__(self, depth: int, game_tree: GameTree) -> None:
+        """Initialize this player.
+
+        Preconditions:
+            - depth >= 1
+            - game_tree represents a game tree at the initial state (root is '*')
+        """
+        self._depth = depth
+
+    def make_move(self, game: ChessGame, previous_move: Optional[str]) -> str:
+        """Make a move given the current game.
+
+        previous_move is the opponent player's most recent move, or None if no moves
+        have been made.
+
+        Preconditions:
+            - There is at least one valid move for the given game
+        """
+        # TODO: implement the method
+
+
+if __name__ == '__main__':
+    import python_ta.contracts
+    python_ta.contracts.check_all_contracts()
+
+    import doctest
+    doctest.testmod()
+
+    import python_ta
+    python_ta.check_all(config={
+        'max-line-length': 100,
+        'disable': ['E1136'],
+    })
