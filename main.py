da66f6d7e03c1a6aea92fe767a1c382dcfd705e3
Junru <71200653+TracyJL@users.noreply.github.com>
2021-04-13 23:51:09 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-"..."
+"""CSC111 Final Project: AI Player in Chinese Chess
+
+Module Description
+===============================
+
+This is module contains the code necessary for running the entire program.
+
+Copyright and Usage Information
+===============================
+
+This file is Copyright (c) 2021 Junru Lin, Zixiu Meng, Krystal Miao and Jenci Wei
+"""
+from player import AIBlack
+from visualization import Game
+
+if __name__ == '__main__':
+    # Data Processing Example
+    # TODO: Add a data processing example (using middle_sample,csv)
+    # Interaction Window
+    ai_black_player = AIBlack('data/tree.xml', 3)
+    game = Game(ai_black_player)
+    game.run()
