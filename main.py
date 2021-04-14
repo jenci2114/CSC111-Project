3910d2b42f1cb5934dec4315b76f862fbde688a9
jenci <68970896+jenci2114@users.noreply.github.com>
2021-04-14 01:01:44 -0700

    Finalized screen size

--- main.py
+++ main.py
@@ -27,22 +27,25 @@ def present() -> None:
     print("'3' for an intermediate-level AI (Warning: each step takes 30-600 seconds)")
     print("'4' for a trained novice-level AI (Warning: takes 1 minute to load tree)")
     option = input()
+    print("'True' if you want a background music, 'False' if you do not want a bgm")
+    option2 = input()
 
-    while option not in {'1', '2', '3', '4'}:
+    while option not in {'1', '2', '3', '4'} or option2 not in {'True', 'False'}:
         print('Invalid input. Please try again.')
         option = input()
+        option2 = input()
 
-    if option == 4:
+    if option == 4 and option2 == "True":
+        g = Game(AIBlack('tree.xml', 3), True, True)
+    elif option == 4 and option2 == "False":
         g = Game(AIBlack('tree.xml', 3))
-    else:  # option in {1, 2, 3}
+    elif option in {'1', '2', '3'} and option2 == "True":
+        g = Game(ExploringPlayer(int(option) + 1), True, True)
+    else:
         g = Game(ExploringPlayer(int(option) + 1))
 
     g.run()
 
 
-
 if __name__ == '__main__':
-
-# if you want backgroud music
-# uncomment code b#   present(True)
     present()
